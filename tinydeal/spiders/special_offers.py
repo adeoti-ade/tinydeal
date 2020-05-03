2a853578a309163d68c961d632d07de9e14a8c22
gboyega.a@credpal.com <gboyega.a@credpal.com>
2020-05-03 17:51:13 +0100

    edited the script so that the User Agent changes from scrapy to well recognized one

--- tinydeal/spiders/special_offers.py
+++ tinydeal/spiders/special_offers.py
@@ -5,7 +5,13 @@ import scrapy
 class SpecialOffersSpider(scrapy.Spider):
     name = 'special_offers'
     allowed_domains = ['www.tinydeal.com']
-    start_urls = ['https://www.tinydeal.com/specials.html']
+    # start_urls = ['https://www.tinydeal.com/specials.html']
+
+    def start_requests(self):
+
+        yield scrapy.Request(url= "https://www.tinydeal.com/specials.html", headers= {
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
+            })
 
     def parse(self, response):
         result = response.xpath("//ul[@class='productlisting-ul']/div/li")
@@ -16,6 +22,13 @@ class SpecialOffersSpider(scrapy.Spider):
                 'title': product.xpath(".//a[@class='p_box_title']/text()").get(),
                 'url': response.urljoin(product.xpath(".//a[@class='p_box_title']/@href").get()),
                 'discounted_price': product.xpath(".//div[@class='p_box_price']/span[1]/text()").get(),
-                'price': product.xpath(".//div[@class='p_box_price']/span[2]/text()").get()
+                'price': product.xpath(".//div[@class='p_box_price']/span[2]/text()").get(),
+                'User-Agent': response.request.headers['User-Agent']
             }
         
+        next_page = response.xpath("//a[@class='nextPage']/@href").get()
+
+        if next_page:
+            yield scrapy.Request(url=next_page, callback=self.parse, headers= {
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
+            })
