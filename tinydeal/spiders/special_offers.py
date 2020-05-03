cd0c858e1649c2652b814b53e22a413c9667b164
gboyega.a@credpal.com <gboyega.a@credpal.com>
2020-05-03 17:22:13 +0100

    first commit of a scrappy spider scraping tinydeal special offer first page

---
+++ tinydeal/spiders/special_offers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class SpecialOffersSpider(scrapy.Spider):
+    name = 'special_offers'
+    allowed_domains = ['www.tinydeal.com']
+    start_urls = ['https://www.tinydeal.com/specials.html']
+
+    def parse(self, response):
+        result = response.xpath("//ul[@class='productlisting-ul']/div/li")
+
+        for product in result:
+            
+            yield {
+                'title': product.xpath(".//a[@class='p_box_title']/text()").get(),
+                'url': response.urljoin(product.xpath(".//a[@class='p_box_title']/@href").get()),
+                'discounted_price': product.xpath(".//div[@class='p_box_price']/span[1]/text()").get(),
+                'price': product.xpath(".//div[@class='p_box_price']/span[2]/text()").get()
+            }
+        
